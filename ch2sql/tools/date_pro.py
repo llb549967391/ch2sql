6ba465c61eb5e972dd5f2010e093d8f72dfb2607
rob <549967391@qq.com>
2018-03-27 23:25:58 +0800

    Update date_pro.py
    增加了同义词识别功能
    增加了周为单位日期的识别功能

--- ch2sql/tools/date_pro.py
+++ ch2sql/tools/date_pro.py
@@ -7,10 +7,15 @@ contributor:
 
 This is a script file processing string about date.
 
-这个版本目前只支持阿拉伯数字
+version1.0:这个版本目前只支持阿拉伯数字
+
+version2.0:增加了同义词识别功能
+		   增加了周为单位日期的识别功能
+
 
 """
 from datetime import date
+from ch2sql.tools import synonyms as sym
 import datetime
 import calendar
 
@@ -28,11 +33,17 @@ def ch2date(str_in):
     # return a time object including today's date to variable today
     today = date.today()
 
+    #记录今天是星期几
+    week_day = int(today.strftime('%w'))
+
     # 记录终止日期，默认中止日期为当前日期
     end = today
 
     # initialise beginning date, default to be today
     begin = today
+
+    #同义词阈值
+    threshold = 0.6
 
     # judge the query date type, if isRange equals True, ignore 'end',
     # else both are valid
@@ -56,9 +67,10 @@ def ch2date(str_in):
     if str_in == '':
         return begin, end, isRange, byMon, byYear
 
-    elif str_in.find('昨天') != -1:
+    elif sym.compare(str_in, '昨天') >= threshold:
         begin = today - datetime.timedelta(1)
-    elif str_in.find('前天') != -1:
+
+    elif sym.compare(str_in, '前天') >= threshold:
         begin = today - datetime.timedelta(2)
 
     elif str_in.find('天前') != -1:
@@ -72,38 +84,20 @@ def ch2date(str_in):
 
         str_begin = str_in.find('最近') + 2
 
-        if str_in.find('年') != -1:
-            year = today.year - int(str_in[str_begin: str_in.find('年')])
+        str_in = str_in[str_begin:]
+
+        t_split = sym.seg(str_in)[0]
+
+        number = int(t_split[0])
+
+        cha = t_split[-1]
+
+        if sym.compare(cha, '年') >= threshold:
+            year = today.year - number
             begin = begin.replace(year, 1, 1)
 
-        elif str_in.find('个') != -1:
-
-            str_end = str_in.find('个')
-            number = int(str_in[str_begin: str_end])
-
-            if str_in.find('季度') != -1:
-
-                season = (today.month - 1) // 3 - number
-                year = today.year
-                month = season * 3 + 1
-                while month < 0:
-                    year -= 1
-                    month += 12
-
-                begin = begin.replace(year, month, 1)
-
-            elif str_in.find('月') != -1:
-
-                year = today.year
-                month = today.month - number
-                while month < 0:
-                    year -= 1
-                    month += 12
-                begin = begin.replace(year, month, 1)
-
-        elif str_in.find('季度') != -1:
-
-            season = (today.month - 1) // 3 - int(str_in[str_begin: str_in.find('季度')])
+        if sym.compare(cha, '季度') >= threshold:
+            season = (today.month - 1) // 3 - number
             year = today.year
             month = season * 3 + 1
             while month < 0:
@@ -112,38 +106,44 @@ def ch2date(str_in):
 
             begin = begin.replace(year, month, 1)
 
-
-        elif str_in.find('月') != -1:
+        if sym.compare(cha, '月') >= threshold:
             year = today.year
-            month = today.month - int(str_in[str_begin: str_in.find('月')])
+            month = today.month - number
             while month < 0:
                 year -= 1
                 month += 12
             begin = begin.replace(year, month, 1)
 
-        elif str_in.find('天') != -1:
-            days = int(str_in[str_begin: str_in.find('天')])
-            begin = today - datetime.timedelta(days)
+        if (sym.compare(cha, '周') >= threshold) | (sym.compare(cha, '礼拜') >= threshold):
+            begin = today - datetime.timedelta(number * 7 + week_day)
 
-            # 年月日类型日期处理
+        if sym.compare(cha, '天') >= threshold:
+            begin = today - datetime.timedelta(number)
+
+
+
+    # 年月日类型日期处理
     elif str_in.find('年') != -1:
 
         isRange = True
 
+        year_loc = str_in.find('年') + 1
+
+        year_part = str_in[0:year_loc]
+
         # 年的特殊情况处理
-        if (str_in.find('今年') != -1) | (str_in.find('该年') != -1) | (str_in.find('本年') != -1) | (
-                str_in.find('这年') != -1) | (str_in.find('这一年')):
+        if (sym.compare(year_part, '今年') >= threshold) | (str_in.find('该年') != -1) | (str_in.find('这一年')):
             year = today.year
 
-        elif str_in.find('去年') != -1:
+        elif sym.compare(year_part, '去年') >= threshold:
             year = today.year - 1
 
-        elif (str_in.find('前年') != -1) | (str_in.find('前一年') != -1):
+        elif sym.compare(year_part, '前年') >= threshold:
             year = today.year - 2
 
         # 年的通用情况处理
         else:
-            year = int(str_in[0:str_in.find('年')])
+            year = int(str_in[0:year_loc - 1])
 
         if str_in.find('月') == -1:
 
@@ -153,7 +153,8 @@ def ch2date(str_in):
 
         else:
 
-            month = int(str_in[str_in.find('年') + 1: str_in.find('月')])
+            month_loc = str_in.find('月') + 1
+            month = int(str_in[year_loc : (month_loc - 1)])
 
             if (str_in.find('日') == -1) & (str_in.find('号') == -1):
 
@@ -165,9 +166,9 @@ def ch2date(str_in):
             else:
 
                 if str_in.find('日') != -1:
-                    day = int(str_in[str_in.find('月') + 1: str_in.find('日')])
+                    day = int(str_in[month_loc: str_in.find('日')])
                 elif str_in.find('号') != -1:
-                    day = int(str_in[str_in.find('月') + 1: str_in.find('号')])
+                    day = int(str_in[month_loc: str_in.find('号')])
 
                 isRange = False
                 begin = begin.replace(year, month, day)
@@ -189,6 +190,9 @@ def ch2date(str_in):
             month = season * 3 + 1
 
             begin = begin.replace(year, month, 1)
+
+        if (str_in.find('周') != -1) | (str_in.find('星期') != -1) | (str_in.find('礼拜') != -1):
+            begin = today - datetime.timedelta(week_day)
 
 
     elif str_in.find('上') != -1:
@@ -228,6 +232,10 @@ def ch2date(str_in):
 
             end = end.replace(year, month, day)
 
+        if (str_in.find('周') != -1) | (str_in.find('星期') != -1) | (str_in.find('礼拜') != -1):
+            begin = today - datetime.timedelta(7 + week_day)
+            end = today - datetime.timedelta(week_day)
+
 
     elif str_in.find('前') != -1:
         isRange = True
@@ -263,6 +271,9 @@ def ch2date(str_in):
 
             end = end.replace(year, month, day)
 
+        if (str_in.find('周') != -1) | (str_in.find('星期') != -1) | (str_in.find('礼拜') != -1):
+            begin = today - datetime.timedelta(14 + week_day)
+            end = today - datetime.timedelta(7 + week_day)
 
     # 没有年的情况处理
     elif str_in.find('月') != -1:
@@ -296,8 +307,9 @@ def ch2date(str_in):
     return begin, end, isRange, byMon, byYear
 
 
+#测试模块
 if __name__ == '__main__':
-    str_in = '30天前'
+    str_in = '前'
 
     begin, end, isRange, byMon, byYear = ch2date(str_in)
 
